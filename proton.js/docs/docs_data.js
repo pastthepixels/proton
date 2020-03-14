16958715695959003212bd12b0bface171790d82
pastthepixels <52388215+pastthepixels@users.noreply.github.com>
2020-03-13 22:30:38 -0600

    AAAAAAAAAAAAAAAdded docs.
    Well, at least I begun adding them.

---
+++ proton.js/docs/docs_data.js
@@ -0,0 +1,109 @@
+var pickledocs_data = {
+	objects: [
+		{
+			name: "Proton3DObject (universal)",
+			description: "An object in Proton3D. It can be an imported object, a light, camera, or mesh.",
+			use: "new Proton3DObject( { type: (string; ['cube', 'sphere', 'cylinder' ... ]) } )",
+			
+			properties: [
+				{
+					//The minimum of what you should have
+					name: "...",
+					type: "...",
+					description: "..."
+				}
+			],
+			functions: [
+				{
+					//The minimum of what you should have
+					name: "...( ... (?), ... (?) )",
+					description: "..."
+				}
+			],
+		},
+		{
+			name: "Proton3DObject (mesh)",
+			description: "An object in Proton3D. It can either be a cube, sphere, or cylinder.",
+			use: "new Proton3DObject( { type: (string; ['cube', 'sphere', 'cylinder']) } )",
+			
+			properties: [
+				{
+					//The minimum of what you should have
+					name: "...",
+					type: "...",
+					description: "..."
+				}
+			],
+			functions: [
+				{
+					//The minimum of what you should have
+					name: "...( ... (?), ... (?) )",
+					description: "..."
+				}
+			],
+		},
+		{
+			name: "Proton3DObject (imported)",
+			description: "An object in Proton3D. It can either be imported via a '.obj' file or a glTF file.",
+			use: "protonjs.importObject( ... )",
+			
+			properties: [
+				{
+					//The minimum of what you should have
+					name: "...",
+					type: "...",
+					description: "..."
+				}
+			],
+			functions: [
+				{
+					//The minimum of what you should have
+					name: "...( ... (?), ... (?) )",
+					description: "..."
+				}
+			],
+		},
+		{
+			name: "Proton3DObject (camera)",
+			description: "A camera in Proton3D. It can either be an orthographic or perspective camera.",
+			use: "new Proton3DObject( { type: (string; ['orthographiccamera', 'perspectivecamera' ]) } )",
+			
+			properties: [
+				{
+					//The minimum of what you should have
+					name: "...",
+					type: "...",
+					description: "..."
+				}
+			],
+			functions: [
+				{
+					//The minimum of what you should have
+					name: "...( ... (?), ... (?) )",
+					description: "..."
+				}
+			],
+		},
+		{
+			name: "Proton3DObject (light)",
+			description: "An light in Proton3D. It can be a point light or a directional light.",
+			use: "new Proton3DObject( { type: (string; ['pointlight', 'directionallight' ]) } )",
+			
+			properties: [
+				{
+					//The minimum of what you should have
+					name: "...",
+					type: "...",
+					description: "..."
+				}
+			],
+			functions: [
+				{
+					//The minimum of what you should have
+					name: "...( ... (?), ... (?) )",
+					description: "..."
+				}
+			],
+		}
+	]
+}
